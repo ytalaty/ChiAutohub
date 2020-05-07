10ec9bdbb17bbcbe3d4c8996cfb02361b0f468f3
Yusuf Talaty <ytalaty@gmail.com>
2020-05-07 17:15:01 -0500

    Fixed minor search error

--- ChiAutohub/routes/search.js
+++ ChiAutohub/routes/search.js
@@ -19,4 +19,22 @@ router.get('/', function(req, res, next) {
 	});
 });
 
+// ==================================================
+// Route to display search
+// ==================================================
+router.post('/', function(req, res, next) {
+
+	let query = "SELECT vehicle_id, vehicle_name, vehicle_description, vehicle_amount FROM vehicles WHERE vehicle_description LIKE '%" + req.body.searchcriteria + "%'";   
+
+	// execute query
+	db.query(query, (err, result) => {
+		if (err) {
+			console.log(err);
+			res.render('error');
+		} else {
+			res.render('search', {allrecs: result});
+		} 
+	});
+});
+
 module.exports = router;
